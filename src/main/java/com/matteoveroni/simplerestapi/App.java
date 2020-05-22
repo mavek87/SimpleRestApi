1498df568911b0ec6b04c7a229dccc8c68d0643e
Matteo Veroni <matver87@gmail.com>
2020-05-22 23:13:12 +0200

    second commit

---
+++ src/main/java/com/matteoveroni/simplerestapi/App.java
@@ -0,0 +1,12 @@
+package com.matteoveroni.simplerestapi;
+
+public class App {
+
+    public static final void main(String... args) {
+        new App().start();
+    }
+
+    public void start() {
+        System.out.println("Hello World");
+    }
+}
