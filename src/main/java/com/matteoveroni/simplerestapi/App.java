59de03a41c8fb694973426ae9fffe2845b48fb21
Matteo Veroni <matver87@gmail.com>
2020-05-22 23:19:11 +0200

    Add log system using slf4j and logback

--- src/main/java/com/matteoveroni/simplerestapi/App.java
+++ src/main/java/com/matteoveroni/simplerestapi/App.java
@@ -1,12 +1,17 @@
 package com.matteoveroni.simplerestapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+    private final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static final void main(String... args) {
         new App().start();
     }
 
     public void start() {
-        System.out.println("Hello World");
+        LOG.debug("Hello World");
     }
 }
